be56e5f69c12c4ffae2ce557ea4ab7a43559abeb
minyoung Ahn <anrh0213@gmail.com>
2022-08-04 22:42:57 +0900

    + 로그인1 마지막

---
+++ src/main/java/hello/login/web/SessionConst.java
@@ -0,0 +1,12 @@
+package hello.login.web;
+
+public class SessionConst {
+    // 인터페이스는 기본이 public
+    public static final String LOGIN_MEMBER = "loginMember";
+
+//    public interface SessionConst {
+//        // 인터페이스는 기본이 public
+//        String LOGIN_MEMBER = "loginMember";
+//    }
+
+}
